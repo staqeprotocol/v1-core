76da6ea70e25e18024ba1f35aa533c5350ad0ec0
Peace Duck <mypeaceduck@proton.me>
2024-03-16 19:47:54 +0200

    Add IERC7572 interface

---
+++ contracts/interfaces/IERC7572.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+pragma abicoder v2;
+
+/** 
+ * @title IERC7572 Interface
+ * @dev Interface for the EIP-7572 standard for defining contract-level metadata.
+ *      https://eips.ethereum.org/EIPS/eip-7572
+ */
+interface IERC7572 {
+    /**
+     * @notice Retrieves the contract-level metadata URI.
+     * @dev Function to get the URI of the contract-level metadata.
+     *      This metadata URI can point to a JSON file that conforms to the "Contract Metadata JSON Schema".
+     * @return The URI string of the contract metadata.
+     */
+    function contractURI() external view returns (string memory);
+
+    /**
+     * @notice Emitted when the contract URI is updated.
+     * @dev This event is emitted when the contract's metadata URI is updated.
+     */
+    event ContractURIUpdated();
+}
---
+++ contracts/interfaces/IStaqeERC7572.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+pragma abicoder v2;
+
+import {IERC7572} from "@staqeprotocol/v1-core/contracts/interfaces/IERC7572.sol";
+
+/**
+ *       _                     _____ ____   ____ _____ ____ _____ ____  
+ *   ___| |_ __ _  __ _  ___  | ____|  _ \ / ___|___  | ___|___  |___ \ 
+ *  / __| __/ _` |/ _` |/ _ \ |  _| | |_) | |      / /|___ \  / /  __) |
+ *  \__ \ || (_| | (_| |  __/ | |___|  _ <| |___  / /  ___) |/ /  / __/ 
+ *  |___/\__\__,_|\__, |\___| |_____|_| \_\\____|/_/  |____//_/  |_____|
+ *                   |_|                                                
+ */
+interface IStaqeERC7572 is IERC7572 {}
